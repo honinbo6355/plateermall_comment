feb630dfb1e88cd5968ca78ef33272e97b7b50e0
nayunjoo <nayj124@plateer.com>
2020-03-23 17:30:55 +0900

    spring boot db config

--- shoppingmall_comment/shoppingmall_comment-boot/src/main/java/com/plateer/ShoppingmallCommentBootApplication.java
+++ shoppingmall_comment/shoppingmall_comment-boot/src/main/java/com/plateer/ShoppingmallCommentBootApplication.java
@@ -3,11 +3,15 @@ package com.plateer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class ShoppingmallCommentBootApplication {
 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(ShoppingmallCommentBootApplication.class, args);
+		log.info("HELLO");
 	}
 }
