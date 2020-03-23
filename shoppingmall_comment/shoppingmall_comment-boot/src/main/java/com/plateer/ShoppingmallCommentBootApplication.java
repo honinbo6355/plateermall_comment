71e08c3a612ef8a08dcdeaa0f95b93645e2802b3
nayunjoo <nayj124@plateer.com>
2020-03-23 16:38:05 +0900

    spring boot commit

---
+++ shoppingmall_comment/shoppingmall_comment-boot/src/main/java/com/plateer/ShoppingmallCommentBootApplication.java
@@ -0,0 +1,13 @@
+package com.plateer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallCommentBootApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ShoppingmallCommentBootApplication.class, args);
+	}
+}
--- shoppingmall_comment/shoppingmall_comment-boot/src/test/java/com/plateer/AppTest.java
+++ shoppingmall_comment/shoppingmall_comment-boot/src/test/java/com/plateer/AppTest.java
@@ -1,4 +1,4 @@
-package org.shoppingmall_comment.boot;
+package com.plateer;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
